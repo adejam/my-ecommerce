e8b12fc5a0ed02d1c01482137cfc4158ea6092ed
Adejam <oluwaloseyifunmiadeleye1@gmail.com>
2021-02-22 00:22:24 +0100

    feat: add feature to remember user after login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,15 +15,20 @@
                 <div class="input-div pass">
                     <div class="div">
                         <Label for="email">Email</Label>
-                        <input id="email" value="{{old('email')}}" name="email"
-                            type="email" class="input" autocomplete="email">
+                        <input id="email" value="{{ old('email') }}" name="email" type="email" class="input"
+                            autocomplete="email">
                     </div>
                 </div>
             </div>
 
             <x-input-without-borders
                 :input="['value' => '', 'name' => 'password', 'title' => 'Password', 'type' => 'password']" />
-
+            <div class="mt-3 mb-1" style="text-align: left; color: #999;">
+                <label for="remember_me" class="flex items-center">
+                    <input id="remember_me" type="checkbox" class="form-checkbox" name="remember">
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                </label>
+            </div>
             <input type="submit" class="btn" value="Login">
             <div class="form-footer">
                 <a href="{{ route('register') }}">Create account</a>
